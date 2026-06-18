0991ccf426ccc52b7ec02433d0faf6f8d6b4c354
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-06-18 12:47:03 +0545

    Avoid image lint warning for QR data URL

--- src/components/InvitePanel.tsx
+++ src/components/InvitePanel.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable @next/next/no-img-element */
+
 import { useEffect, useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
