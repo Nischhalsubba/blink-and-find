8da59887604ba01e7ca8eff623495d7befeb3936
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-07-12 09:38:51 +0545

    Fix Next 16 ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,14 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      ".next/**",
-      "out/**",
-      "node_modules/**",
-      "coverage/**",
-    ],
-  },
-];
-
-export default eslintConfig;
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "node_modules/**",
+    "coverage/**",
+  ]),
+]);
